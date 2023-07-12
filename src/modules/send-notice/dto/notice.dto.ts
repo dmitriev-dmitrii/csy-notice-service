eca0f1387a6894faf37a710e5727b06a6231a42d
alibekov <alibekov>
2023-07-12 15:04:44 +0300

    Changed env

--- src/modules/send-notice/dto/notice.dto.ts
+++ src/modules/send-notice/dto/notice.dto.ts
@@ -2,4 +2,5 @@ export class NoticeDto {
   user: string | number; // phone / email
   noticeType: string; // wasap //email
   template: string | number; //id or name
+  text?: string; // what text to send
 }
